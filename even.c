266899ece5161d53e74339b71966697030677f53
RENUKA PRASAD S B <43398912+rpsb@users.noreply.github.com>
2018-09-23 22:30:31 +0530

    Update even.c

--- even.c
+++ even.c
@@ -4,8 +4,8 @@ void even()
 {
 	int a;
 	printf("enter a number\n");
-	scanf("%d",&a)
-	if(a%2==0)
+	scanf("%d",&a);
+	if(a % 2 == 0)
 	printf("entered number is even\n");
 	else
 	printf("entered number is odd\n");
